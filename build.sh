007c6c8cc17e89eaa667e44c067367a6b64bf1a2
Nick Keuning <nicholaskeuning@gmail.com>
2017-11-20 13:46:17 -0800

    Minor updates to build.sh to  remove hidden files from tar.gz so splunk will take the packages for certification.

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ fi
 tmp_ver=($(cat ./pvs/default/app.conf | grep version | tr "=" " "))
 version=${tmp_ver[1]}
 tar_name="pvs-${version}.tar.gz"
-tar -zcf ${tar_name} pvs
+find ./pvs/ -name '.*' -exec rm -f {} \;
+COPYFILE_DISABLE=1 tar -zcf ${tar_name} pvs/ 
